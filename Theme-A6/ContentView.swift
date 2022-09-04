abb450e85010353e63983d6bd4a0a8e44248516b
Kyle Sullivan <kyles112@gmail.com>
2022-09-03 20:50:08 -0700

    Initial Commit

---
+++ Theme-A6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Theme-A6
+//
+//  Created by user224517 on 9/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Theme-A6/Theme_A6App.swift
@@ -0,0 +1,17 @@
+//
+//  Theme_A6App.swift
+//  Theme-A6
+//
+//  Created by user224517 on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Theme_A6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
